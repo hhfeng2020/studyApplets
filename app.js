92e0a60e4983f958bedbc9acb157e842706a8305
hhfeng2020 <hhfeng2018@163.com>
2020-11-27 17:59:55 +0800

    APP与页面注册

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+//注册App
 App({
 
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
-  onLaunch: function () {
-    
+  onLaunch: function (options) {
+    wx.getUserInfo({
+      success: function (res) {
+        console.log("用户信息获取方式一(可能废弃)")
+        console.log(res);
+      }
+    })
   },
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
-    
+    console.log('场景值: ' + options.scene);//其他生命周期函数都有【options】参数
   },
 
   /**
@@ -26,5 +32,8 @@ App({
    */
   onError: function (msg) {
     
+  },
+  globalData: {
+    haha: "全局数据"
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,14 @@
-// pages/home/home.js
+const app = getApp();//获取App对象
+var haha = app.globalData.haha;
+console.log(haha);
+
+//注册页面
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleGetUserInfo: function (event) {
+    console.log('用户信息获取方式二');
+    console.log(event);
   }
 })
